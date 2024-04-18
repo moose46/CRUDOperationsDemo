174c0230c0e2f8cf4a51f056fc76a1ae3bdb2761
me <me@robertcurtiss.com>
2024-04-18 16:19:27 -0400

    Initial Udemy

---
+++ PayRollApp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+class Employee(models.Model):
+    COUNTRIES = [
+        ("IND", "India"),
+        ("USA", "United States of America"),
+        ("UK", "United Kingdom"),
+        ("AUS", "Australia"),
+        ("AU", "Australia"),
+        ("SP", "Spain"),
+    ]
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    TitleName = models.CharField(max_length=30)
+    HasPasport = models.BooleanField()
+    Salary = models.IntegerField()
+    BirthDate = models.DateField()
+    HireDate = models.DateField()
+    Notes = models.CharField(max_length=200)
+    Country = models.CharField(max_length=35, choices=COUNTRIES, default=None)
